6ed45443d703d5248346f7bf0b9b850bdf91cf29
Kuukey <knightwolf152@gmail.com>
2024-05-20 13:06:24 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Equipment from './pages/Equipment';
+import Loadout from './pages/Loadout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/Equipment" component={Equipment}/>
+          <Route exact path="/Loadout" component={Loadout}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
---
+++ src/pages/Equipment.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Equipment() {
+    return <h1>Equipment Page</h1>;
+}
+
+export default Equipment;
